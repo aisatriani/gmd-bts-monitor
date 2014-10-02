d2fe27ab56d854d2e8b4a3674fb9bd12e8b23920
aisatriani <>
2014-10-02 20:33:12 +0800

    main

---
+++ src/main/java/com/ais/gmdapp/bts/StartServer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ais.gmdapp.bts;
+
+/**
+ *
+ * @author Asus
+ */
+public class StartServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
